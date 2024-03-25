b7a3d85bb580c9c459dcad5e261b25f5d6d43c86
nicelkaim <69821634+nicelkaim@users.noreply.github.com>
2024-03-25 18:11:43 -0400

    Update mongoosecluster.tf

--- terraform/mongoosecluster.tf
+++ terraform/mongoosecluster.tf
@@ -1,8 +1,17 @@
 resource "google_container_cluster" "mongoose" {
   name     = "mongoose"
   location = "us-central1-a"
-  initial_node_count       = 2
-  cluster_autoscaling {
-    enabled = true
+  remove_default_node_pool = true
+  initial_node_count       = 1
+}
+
+resource "google_container_node_pool" "mongoose_nodes" {
+  name       = "mongoose-nodepool"
+  location   = "us-central1-a"
+  cluster    = google_container_cluster.mongoose.name
+  node_count = 4
+  autoscaling {
+    min_node_count = 1
+    max_node_count = 5
   }
 }
