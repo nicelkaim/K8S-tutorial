6c52b611eb9562595ddf720fc23961f493d188ba
nicelkaim <69821634+nicelkaim@users.noreply.github.com>
2024-04-01 10:23:27 -0400

    Update narwhalcluster.tf

--- terraform/narwhalcluster.tf
+++ terraform/narwhalcluster.tf
@@ -1,10 +1,13 @@
-resource "google_container_cluster" "narwhal" {
-  name     = "narwhal"
+resource "google_container_cluster" "http" {
+  name     = "http"
   location = "us-central1-a"
   initial_node_count       = 3
   network_policy {
     enabled = true
   }
+  addons_config {
+    http_load_balancing {
+      disabled = true
+    }  
+  }
 }
- 
-
